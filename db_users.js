99affb5cc1dd318a0922a544cc119116f5b2d502
Navaneeth1510 <117336493+Navaneeth1510@users.noreply.github.com>
2024-09-29 09:49:24 +0530

    Update db_users.js

--- db_users.js
+++ db_users.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 let dbConnection
-let uri='mongodb+srv://NavaneethN:N%40va1510av%40N@mindmender.s2p1k4t.mongodb.net/?retryWrites=true&w=majority&appName=MindMender'
+let uri='mongodb+srv://NavaneethN:<password>@mindmender.s2p1k4t.mongodb.net/?retryWrites=true&w=majority&appName=MindMender'
 let dbname = 'Mind'
 module.exports={
     connectToDb:(cb)=>{
